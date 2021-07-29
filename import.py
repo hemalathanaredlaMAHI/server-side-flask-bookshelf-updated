56912df1dce2b34887f75e99c7ac9afedbb9cc4a
hemalathanaredlaMAHI <41735211+hemalathanaredlaMAHI@users.noreply.github.com>
2021-07-29 13:54:35 +0530

    version 1

---
+++ import.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from models import Book
+import csv
+
+
+engine= create_engine(os.getenv("DATABASE_URL"))
+db_session = scoped_session(sessionmaker(bind=engine))
+with open('books.csv') as csvfile:
+    readCSV = csv.reader(csvfile,delimiter=',')
+    for row in readCSV:
+        new_book= Book(isbn=row[0],name=row[1],author=row[2], year=row[3])
+        db_session.add(new_book)
+db_session.commit()
+db_session.close()
+
